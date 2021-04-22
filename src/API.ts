7330507b6519b49808bf6079c08575bfcc575941
lwswr <lewiswear@msn.com>
2021-04-22 22:28:32 +0100

    added another get request to info

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Categories } from "./appSlice";
 
 export type DataResponse = {
   error: boolean;
@@ -25,11 +26,47 @@ export type Joke = {
   setup: string;
 };
 
-export const getJokes = async (search: string) => {
+export type SafeJokesCount = {
+  lang: string;
+  count: number;
+};
+
+// copied response object and created type
+export type Info = {
+  error: boolean;
+  version: string;
+  jokes: {
+    totalCount: number;
+    categories: string[];
+    flags: string[];
+    types: ["single", "twopart"];
+    submissionURL: string;
+    idRange: {
+      cs: number[];
+      de: number[];
+      es: number[];
+      fr: number[];
+      en: number[];
+      pt: number[];
+    };
+    safeJokes: SafeJokesCount[];
+  };
+  formats: string[];
+  jokeLanguages: number;
+  systemLanguages: number;
+  info: string;
+  timestamp: Date;
+};
+
+export const getJokes = async (search: string, category: Categories) => {
   const resultsAmount: number = 10;
   return axios
     .get<DataResponse>(
-      `https://v2.jokeapi.dev/joke/Any?contains=${search}&amount=${resultsAmount}&safe-mode`
+      `https://v2.jokeapi.dev/joke/${category}?contains=${search}&amount=${resultsAmount}&safe-mode`
     )
     .then((res) => res.data);
+};
+
+export const getInfo = async () => {
+  return axios.get<Info>(`https://v2.jokeapi.dev/info`).then((res) => res.data);
 };
