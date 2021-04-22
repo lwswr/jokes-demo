fd76cc94e9ec5a327c83bd49babf8d2c1cf10df1
lwswr <lewiswear@msn.com>
2021-04-22 10:22:47 +0100

    incorporated custom components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getJokes } from "./API";
+import {
+  AppState,
+  categories,
+  categoryOptionUpdated,
+  dataFetched,
+  searchSubmitted,
+} from "./appSlice";
+import { JokeList } from "./JokeList";
+import styled from "styled-components";
+import { Selector } from "./Selector";
+import { SearchForm } from "./SearchForm";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector(selectState);
+
+  useEffect(() => {
+    const getJokesData = async (search: string) => {
+      const jokesData = await getJokes(search);
+      dispatch(dataFetched({ data: jokesData }));
+      console.log(jokesData);
+    };
+    getJokesData(state.search);
+  }, [dispatch, state.search]);
+
+  // compute filtered list which is only rendered when the category "Any" is active
+  const filteredList = state.data?.jokes.filter(
+    (joke) => joke.category === state.category
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Selector
+        selectedCategory={categories}
+        value={state.category}
+        onChange={(category) =>
+          dispatch(categoryOptionUpdated({ selectedCategory: category }))
+        }
+      />
+      <SearchForm
+        submit={(text) => dispatch(searchSubmitted({ text: text }))}
+      />
+      {state.data && state.category !== "Any" ? (
+        filteredList?.length !== 0 ? (
+          <JokeList jokes={filteredList} />
+        ) : (
+          "No jokes from this category"
+        )
+      ) : (
+        <JokeList jokes={state.data?.jokes} />
+      )}
+    </Container>
   );
 }
+
+const selectState = ({ state }: { state: AppState }) => state;
 
 export default App;
