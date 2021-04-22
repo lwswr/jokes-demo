52db1fadf7d82cde41e7573671aed34dfe84d67e
lwswr <lewiswear@msn.com>
2021-04-22 22:31:05 +0100

    created conditional rendering for joke lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,120 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getJokes } from "./API";
+import { getInfo, getJokes, Joke } from "./API";
 import {
   AppState,
-  categories,
-  categoryOptionUpdated,
+  Categories,
+  categorySubmitted,
+  clearButtonClicked,
   dataFetched,
-  searchSubmitted,
+  infoDataFetched,
+  searchTextUpdated,
 } from "./appSlice";
 import { JokeList } from "./JokeList";
 import styled from "styled-components";
-import { Selector } from "./Selector";
 import { SearchForm } from "./SearchForm";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const LeftColumn = styled.div`
+  position: fixed;
+  top: 3.5%;
+  left: 2.5%;
+  width: 18%;
+  height: 30%;
+  display: flex;
   flex-direction: column;
-  align-items: center;
+  justify-content: space-evenly;
+  border: 1px solid lightgrey;
+  border-radius: 0.5rem;
+  padding: 20px;
 `;
 
 function App() {
   const dispatch = useDispatch();
   const state = useSelector(selectState);
 
+  // one call on first render fetches the information
   useEffect(() => {
-    const getJokesData = async (search: string) => {
-      const jokesData = await getJokes(search);
-      dispatch(dataFetched({ data: jokesData }));
-      console.log(jokesData);
+    const getInfoData = async () => {
+      try {
+        const infoData = await getInfo();
+        dispatch(infoDataFetched({ info: infoData }));
+      } catch (error) {
+        console.log(error);
+      }
     };
-    getJokesData(state.search);
-  }, [dispatch, state.search]);
+    getInfoData();
+  }, [dispatch]);
+
+  // fetch from api when category or search is updated and category changes
+  useEffect(() => {
+    const getJokesData = async (search: string, category: Categories) => {
+      try {
+        const jokesData = await getJokes(search, category);
+        dispatch(dataFetched({ data: jokesData }));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getJokesData(state.search, state.category);
+  }, [dispatch, state.search, state.category]);
 
   // compute filtered list which is only rendered when the category "Any" is active
-  const filteredList = state.data?.jokes.filter(
-    (joke) => joke.category === state.category
+  const filterList = (list: Joke[] | undefined, stateCategory: Categories) => {
+    return list?.filter((joke) => joke.category === stateCategory);
+  };
+
+  // filter the results of the request by category
+  const filteredList = state.data
+    ? filterList(state.data?.jokes, state.category)
+    : [];
+
+  // calc sum of safejokes
+  const safeJokesCount = state.info?.jokes.safeJokes.reduce(
+    (accum, item) => accum + item.count,
+    0
   );
 
   return (
     <Container>
-      <Selector
-        selectedCategory={categories}
-        value={state.category}
-        onChange={(category) =>
-          dispatch(categoryOptionUpdated({ selectedCategory: category }))
-        }
-      />
-      <SearchForm
-        submit={(text) => dispatch(searchSubmitted({ text: text }))}
-      />
-      {state.data && state.category !== "Any" ? (
-        filteredList?.length !== 0 ? (
-          <JokeList jokes={filteredList} />
-        ) : (
-          "No jokes from this category"
-        )
-      ) : (
-        <JokeList jokes={state.data?.jokes} />
-      )}
+      <LeftColumn>
+        <div>
+          There are a total of {state.info?.jokes.totalCount} jokes to browse,
+          however seeing as you are in safe mode you can access {safeJokesCount}
+          .
+        </div>
+        <SearchForm
+          submit={(category) =>
+            dispatch(categorySubmitted({ category: category }))
+          }
+          onChange={(text) => {
+            dispatch(searchTextUpdated({ text: text }));
+          }}
+          onClick={() => {
+            dispatch(clearButtonClicked());
+          }}
+        />
+        <div>
+          Displaying {state.data?.jokes ? state.data?.jokes.length : "0"}{" "}
+          results
+          {state.search !== "" ? <span> for "{state.search}"</span> : null} in "
+          {state.category}"
+        </div>
+      </LeftColumn>
+
+      {/* Conditionally render the lsit of jokes based on whether the list is being filtered */}
+      {state.data?.jokes && state.category === "Any" ? (
+        <JokeList jokes={state.data.jokes} />
+      ) : state.category !== "Any" ? (
+        <JokeList jokes={filteredList} />
+      ) : null}
     </Container>
   );
 }
