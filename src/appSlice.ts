511a0dd3df77e8e47e46fefde727cc30a2ddd8f5
lwswr <lewiswear@msn.com>
2021-04-22 22:29:04 +0100

    replaced some reducer functions

--- src/appSlice.ts
+++ src/appSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { DataResponse } from "./API";
+import { DataResponse, Info } from "./API";
 
 // don't require "Dark" category as running API calls in safe mode
 export const categories = [
@@ -14,12 +14,14 @@ export type Categories = typeof categories[number];
 
 export type AppState = {
   data?: DataResponse;
+  info?: Info;
   category: Categories;
   search: string;
 };
 
 const initialState: AppState = {
   data: undefined,
+  info: undefined,
   category: "Any",
   search: "",
 };
@@ -34,22 +36,34 @@ export const appSlice = createSlice({
     ) => {
       state.data = payload.data;
     },
-    categoryOptionUpdated: (
-      state,
-      { payload }: PayloadAction<{ selectedCategory: Categories }>
-    ) => {
-      state.category = payload.selectedCategory;
+    infoDataFetched: (state, { payload }: PayloadAction<{ info: Info }>) => {
+      state.info = payload.info;
     },
-    searchSubmitted: (state, { payload }: PayloadAction<{ text: string }>) => {
+    categorySubmitted: (
+      state,
+      { payload }: PayloadAction<{ category: Categories }>
+    ) => {
+      state.category = payload.category;
+    },
+    searchTextUpdated: (
+      state,
+      { payload }: PayloadAction<{ text: string }>
+    ) => {
       state.search = payload.text;
+    },
+    clearButtonClicked: (state) => {
+      state.search = "";
+      state.category = "Any";
     },
   },
 });
 
 export const {
   dataFetched,
-  categoryOptionUpdated,
-  searchSubmitted,
+  infoDataFetched,
+  categorySubmitted,
+  searchTextUpdated,
+  clearButtonClicked,
 } = appSlice.actions;
 
 export default appSlice.reducer;
