4bc55f23f8fd409cb1e8b0d05ac4bf5030b7ec3b
lwswr <lewiswear@msn.com>
2021-04-22 22:29:55 +0100

    Nested Selector within SearchForm

--- src/SearchForm.tsx
+++ src/SearchForm.tsx
@@ -1,19 +1,102 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { AppState, Categories, categories } from "./appSlice";
+import { Selector } from "./Selector";
 
-export const SearchForm = ({ submit }: { submit: (text: string) => void }) => {
-  const [text, setText] = React.useState<string>("");
+const Form = styled.form`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const TextInput = styled.input`
+  height: 35px;
+  width: 67.5%;
+  font-family: "Nunito", sans-serif;
+  border: 1px solid lightgrey;
+  border-radius: 0.25rem;
+  transition: 0.4s;
+  :hover {
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
+      rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+  }
+  :focus {
+    outline: none;
+  }
+`;
+
+const Button = styled.button`
+  font-size: 1rem;
+  font-family: "Nunito", sans-serif;
+  border: none;
+  border-radius: 0.5rem;
+  background: #1757a0;
+  width: 6rem;
+  height: 2rem;
+  text-decoration: none;
+  padding: 0.25rem;
+  color: white;
+  transition: 0.3s;
+  align-self: flex-end;
+  letter-spacing: 1px;
+  :hover {
+    background: #114177;
+  }
+`;
+
+const Row = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const SearchForm = ({
+  submit,
+  onChange,
+  onClick,
+}: {
+  submit: (category: Categories) => void;
+  onChange: (text: string) => void;
+  onClick: () => void;
+}) => {
+  const { search } = useSelector(selectState);
+  const [category, setCategory] = React.useState<Categories>("Any");
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        submit(text);
-      }}
-    >
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+    <Form>
+      <Selector
+        selectedCategory={categories}
+        value={category}
+        onChange={(category) => {
+          submit(category);
+          setCategory(category);
+        }}
       />
-    </form>
+      <Row>
+        <label>Keyword</label>
+        <TextInput
+          type="text"
+          value={search}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      </Row>
+      <Button
+        onClick={(e) => {
+          e.preventDefault();
+          setCategory("Any");
+          onClick();
+        }}
+      >
+        Clear
+      </Button>
+    </Form>
   );
 };
+
+const selectState = ({ state }: { state: AppState }) => state;
