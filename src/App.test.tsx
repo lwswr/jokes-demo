ddf865fa388cf9004cb40deca5446578e5175831
lwswr <lewiswear@msn.com>
2021-04-26 19:40:13 +0100

    added basic testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("sucessfully renders App", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    const linkElement = screen.getByText(/Submit your own joke!/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  it("renders ten jokes", async () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect((await screen.findAllByTestId("jokeListItem")).length).toBe(10);
+  });
 });
